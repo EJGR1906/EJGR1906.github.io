6e17822496ad832f6efbcbc65cbb7ac33fcc8396
EJGR1906 <eduardogonzalez1333@gmail.com>
2023-01-04 16:04:24 -0400

    commit

---
+++ js/custom-collapse.js
@@ -0,0 +1,7 @@
+function open_collapse() {
+  document.getElementById("open-collapse").id="close-collapse";
+}
+
+function close_collapse() {
+  document.getElementById("close-collapse").id="open-collapse";
+}
